2454302e8187e09db7bd2cdca6c764d600f0425c
roncan <65841016+roncan1@users.noreply.github.com>
2022-04-20 23:13:22 +0900

    activity_study.xml 구현완료

--- app/src/main/java/com/portpolio/aragada/MainActivity.java
+++ app/src/main/java/com/portpolio/aragada/MainActivity.java
@@ -3,13 +3,14 @@ package com.portpolio.aragada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView btn_hiragana_study, btn_hiragana_test, btn_gatakana_study, btn_gatakana_test;
+    ImageView btn_hiragana_study, btn_hiragana_test, btn_gatakana_study, btn_gatakana_test, btn_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         setBtn();
+        search_japanese();
     }
 
 //    카테고리
@@ -60,10 +62,23 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void search_japanese() {
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://ja.dict.naver.com/#/main");
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
     void init() {
         btn_hiragana_study = (ImageView) findViewById(R.id.btn_hiragana_study);
         btn_hiragana_test = (ImageView) findViewById(R.id.btn_hiragana_test);
         btn_gatakana_study = (ImageView) findViewById(R.id.btn_gatakana_study);
         btn_gatakana_test = (ImageView) findViewById(R.id.btn_gatakana_test);
+        btn_search = (ImageView) findViewById(R.id.btn_search);
     }
 }
