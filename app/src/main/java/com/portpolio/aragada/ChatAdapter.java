323bd61488a0ebec64076abc104ecdf6ab384144
roncan <65841016+roncan1@users.noreply.github.com>
2022-06-25 18:17:12 +0900

    tts 기능 완성

--- app/src/main/java/com/portpolio/aragada/ChatAdapter.java
+++ app/src/main/java/com/portpolio/aragada/ChatAdapter.java
@@ -1,18 +1,24 @@
 package com.portpolio.aragada;
 
 import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ChatAdapter extends BaseAdapter {
     Context context = null;
     LayoutInflater layoutInflater = null;
     ArrayList<ChatData> chatData;
+    TextToSpeech tts;
 
     public ChatAdapter(Context context, ArrayList<ChatData> data) {
         this.context = context;
@@ -41,6 +47,17 @@ public class ChatAdapter extends BaseAdapter {
         View userChatView = layoutInflater.inflate(R.layout.chat_user, null);
         TextView tv_daara = daaraChatView.findViewById(R.id.tv_daara);
         TextView tv_user = userChatView.findViewById(R.id.tv_user);
+        ImageButton btn_tts = daaraChatView.findViewById(R.id.btn_chat_tts);
+
+        setTts();
+
+        btn_tts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts.speak(tv_daara.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+                Log.d("터치됨", "onClick: " + tv_daara.getText().toString());
+            }
+        });
 
         if (i % 2 != 0) {
             tv_daara.setText(chatData.get(i).getValue());
@@ -50,5 +67,19 @@ public class ChatAdapter extends BaseAdapter {
             return userChatView;
         }
 
+    }
+
+
+
+    void setTts() {
+        tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.JAPANESE);
+                }
+            }
+        });
     }
 }
